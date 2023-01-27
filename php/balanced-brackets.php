e743c162b1ac747293a4520ff6104ff250d404ca
Greg <gregsatorres@gmail.com>
2023-01-27 14:45:48 -0300

    First Commit

---
+++ php/balanced-brackets.php
@@ -0,0 +1,52 @@
+<?php 
+/**
+ * https://www.hackerrank.com/challenges/balanced-brackets/problem
+ * 
+ * */
+
+function isBalanced($s) {
+    $chaves = str_split($s);
+    $isBalanced = 'YES';
+    $openings = ['{','[','('];
+    $closing = ['}',']',')'];
+    $breakFor = false;
+    $aberturas = [];
+    $cont1 = count($chaves);
+    //{[()]}
+    for($i=0; $i<count($chaves); $i++){
+        if(in_array($chaves[$i], $openings)){
+            array_push($aberturas, $chaves[$i]);
+            continue;
+        }else if(in_array($chaves[$i], $closing)){
+            switch($chaves[$i]){
+                case "}":
+                    if($aberturas[count($aberturas)-1] == "{"){
+                        array_splice($aberturas, (count($aberturas)-1),1);
+                        break;
+                    }
+                    $breakFor = true;
+                    break;
+                case "]":
+                    if($aberturas[count($aberturas)-1] == "["){
+                        array_splice($aberturas, (count($aberturas)-1),1);
+                        break;
+                    }
+                    $breakFor = true;
+                    break;
+                case ")": 
+                    if($aberturas[count($aberturas)-1] == "("){
+                        array_splice($aberturas, (count($aberturas)-1),1);
+                        break;
+                    }
+                    $breakFor = true;
+                    break;
+            }
+            if ($breakFor === true) break;  
+        }
+    }
+    
+    return (count($aberturas) > 0 || $breakFor) ? 'NO' : 'YES'; 
+}
+
+
+ ?>
