17fac68ecd476ea12f82acc6d5bec9b1ca5f5672
Greg Alves Satorres <gregsatorres@gmail.com>
2023-02-11 20:00:15 -0300

    Add 2d array php

---
+++ php/2d-array.php
@@ -0,0 +1,20 @@
+<?php 
+
+	function hourglassSum($arr) {
+	    $sum = 0;
+	    
+	    for($i=0; $i < 4; $i++){
+	        for($j=0; $j< 4;$j++){
+	            $sum += $arr[$i][$j] + $arr[$i][$j+1] + $arr[$i][$j+2];
+	            $sum += $arr[$i+1][$j+1];
+	            $sum += $arr[$i+2][$j] + $arr[$i+2][$j+1] + $arr[$i+2][$j+2];
+	            if($sum > $bigger){
+	                $bigger = $sum;
+	            }
+	            $sum = 0;
+	        }
+	    }
+	    return $bigger;
+	}
+
+ ?>
